99c8170f817b4ad5573c66cc26e5a353decee501
Evantha Manikpura <evantha.manikpura@cinglevue.com>
2020-03-31 09:48:35 +0530

    one way data binding

--- the-study-mate/src/app/courses/course/course.component.ts
+++ the-study-mate/src/app/courses/course/course.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-course',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
 
-  constructor() { }
+  course = {
+    title: 'Angular Workshop',
+    price: 50,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!',
+    starRating: 4
+  }
+
+  constructor() {}
 
   ngOnInit(): void {
+/*     setInterval(() => {
+      this.course.price = Math.round(Math.random() * 100);
+    }, 1000); */
+  }
+
+  onClick(event: Event) {
+    event.preventDefault();
+    console.log('clicked');
+    this.course.price += 10;
   }
 
 }
